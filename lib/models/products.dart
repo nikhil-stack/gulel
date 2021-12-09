e3f345b0fe6a94b7f0dcd0b6329fc6e3772a7bc8
nikhil-stack <56017618+nikhil-stack@users.noreply.github.com>
2021-12-10 01:00:29 +0530

    product model added

---
+++ lib/models/products.dart
@@ -0,0 +1,14 @@
+class Product {
+  String id;
+  String title;
+  double price;
+  String imageUrl;
+  int stockAvailable;
+  Product({
+    this.id,
+    this.title,
+    this.price,
+    this.imageUrl,
+    this.stockAvailable,
+  });
+}
