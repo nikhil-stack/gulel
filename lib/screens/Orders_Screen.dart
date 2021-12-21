7afb514b10494142ff4c6380389488cd798dce93
Vikas Pandey <vikaspandey17730@gmail.com>
2021-12-22 01:10:25 +0530

    orderScreenDone

--- lib/screens/Orders_Screen.dart
+++ lib/screens/Orders_Screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gulel/Providers/Order_Provider.dart';
+import 'package:gulel/widgets/order_item.dart';
+import 'package:provider/provider.dart';
 
 class OrderScreen extends StatefulWidget {
   static const routeName = '\Order_Screen';
@@ -7,12 +10,29 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
+  var _Isloading = false;
+  @override
+  void initState() {
+    _Isloading = true;
+
+    Provider.of<Orders>(context, listen: false).getandset().then((_) {
+      setState(() {
+        _Isloading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Order1 = Provider.of<Orders>(context);
     return Scaffold(
-      body: Center(
-        child: Text("Orders_Screen"),
-      ),
+      body: _Isloading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemBuilder: (ctx, index) => OrderItem1(Order1.Order[index]),
+              itemCount: Order1.Order.length,
+            ),
     );
   }
 }
